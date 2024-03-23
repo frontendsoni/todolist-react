f9a47606c4ee1cb974c3e3578626c276c004deca
Gaurav Kumar <frontendsoni@gmail.com>
2024-03-23 16:28:13 +0530

    add task completion functionality

--- src/components/taskbox.jsx
+++ src/components/taskbox.jsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
-const TaskBox = ({task}) => {
+
+const TaskBox = ({task, completeTaskFunction}) => {
 
 
-
+const handleTaskCompletion = (taskId) => {
+  const completedTask = task.find(item => item.id === taskId);
+  if (completedTask) {
+    completeTaskFunction({ ...completedTask, completed: !completedTask.completed });
+  }
+}
   return (
     <div className="task_box" id="scrollable-div">
       <ul className="task_list">
         {task.map( (item, index) => {
-          const {text, completed, date} = task;
+          console.log(item) 
+          const {id, text, completed, date} = item;
           return (<li key={index} id={index}>
                     <div className="todo_content">
-                      <span className="checkbox {completed ? 'completed' : ''} "></span>
-                      <p className={completed ? 'completed' : ''} >{item}</p>
+                      <span className= {completed ? 'tick checkbox' : 'checkbox'} onClick={() => handleTaskCompletion(id)} ></span>
+                      <p className={completed ? 'completed' : ''} >{text}</p>
                     </div>
                     <div className="update_content">
                       <span className="task_date">{date}</span>
--- src/components/taskrecord.jsx
+++ src/components/taskrecord.jsx
@@ -1,59 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TaskBox from "./taskbox";
 
 const TaskRecord = () => {
   const [inputValue, setInputValue] = useState('');
   const [tasks, setTasks] = useState([]);
 
-  
+  const saveTodoList = (todoList) => {
+    localStorage.setItem('tasks', JSON.stringify(todoList));
+  }
+
+  const taskCompleted =(completedTaskList) => {
+    console.log(completedTaskList);
+    const updatedTasks = tasks.map(item => item.id === completedTaskList.id ? completedTaskList : tasks);
+    setTasks(updatedTasks);
+    saveTodoList(updatedTasks);
+  }
+
   const handleAddTask = () => {
     if (inputValue.trim() !== '') {
-
       const currentDate = new Date();
       const day = currentDate.getDate();
-      const month = currentDate.getMonth() + 1; 
+      const month = currentDate.getMonth() + 1;
       const year = currentDate.getFullYear();
       const formattedDate = `${day}/${month}/${year}`;
 
       const newTask = {
-        text:inputValue,
-        completed:false,
-        date:formattedDate
-      }
+        id: tasks.length + 1,
+        text: inputValue,
+        completed: false,
+        date: formattedDate
+      };
 
-      const updatedTask = [...tasks, newTask]
-      setTasks(updatedTask);
+      const updatedTasks = [...tasks, newTask];
+      setTasks(updatedTasks);
 
-      localStorage.setItem('tasks', JSON.stringify('updatedTasks'));
+      saveTodoList(updatedTasks);
       setInputValue('');
     }
   };
 
   useEffect(() => {
-
     const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
     setTasks(storedTasks);
-
   }, []);
 
   return (
     <div className="task_record">
       <div className="input_box">
-
         <input
           type="text"
           className="add_task"
           placeholder="Add task..."
           value={inputValue}
-          onChange={(e) =>setInputValue(e.target.value)}
+          onChange={(e) => setInputValue(e.target.value)}
         />
-
         <div className="add_btn" onClick={handleAddTask}>
-          <i className="fas fa-plus" ></i>
+          <i className="fas fa-plus"></i>
         </div>
-
       </div>
-      <TaskBox task = {tasks} />
+      <TaskBox task={tasks} completeTaskFunction = {taskCompleted} />
     </div>
   );
 };
